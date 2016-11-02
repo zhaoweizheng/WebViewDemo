612ed7fe5a3a4423176a879402e2d90c32209afc
David <zhaoweizheng66@163.com>
2016-11-02 22:55:08 +0800

    tableView嵌套webView

---
+++ CollectionViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionViewDemo
+//
+//  Created by apple on 16/11/2.
+//  Copyright © 2016年 赵伟争. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollectionViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionViewDemo
+//
+//  Created by apple on 16/11/2.
+//  Copyright © 2016年 赵伟争. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
